a23a2ac2c53e28c895224289011136adcc2d0ae8
Bendi11 <bkliebmann@gmail.com>
2021-11-28 18:49:45 -0800

    Added Files structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+pub mod util;
+pub mod parse;
 
 fn main() {
-    
+
 }
---
+++ src/util/files.rs
@@ -0,0 +1,35 @@
+//! Module containing the [Files] structure that holds an arena of [CompiledFile] structures
+
+use std::path::PathBuf;
+
+/// A structure containing all data from a compiled spark source file needed by the compiler
+/// for location information
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct CompiledFile {
+    pub path: PathBuf,
+}
+
+/// Container holding the data of all files being compiled by sparkc
+#[derive(Clone, Debug, Default)]
+pub struct Files {
+    /// A dynamic array of all files that are being compiled
+    files: Vec<CompiledFile>,
+}
+
+/// An identifier for a certain [CompiledFile] in a [Files] structure
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct FileId(u16);
+
+impl Files {
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Add a new file info structure and return the ID of the inserted file
+    pub fn add(&mut self, data: CompiledFile) -> FileId {
+        self.files.push(data);
+        FileId((self.files.len() - 1) as u16)
+    }
+}
+
