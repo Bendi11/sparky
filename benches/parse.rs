5ca6229e7d314299979a4f0e32a96227be70dbe0
Bendi11 <bkliebmann@gmail.com>
2021-11-28 18:17:24 -0800

    Removed old files

---
+++ benches/parse.rs
@@ -0,0 +1,8 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn benchmark(c: &mut Criterion) {
+    c.bench_function("Parse 100 LOC", move |_| {});
+}
+
+criterion_group!(lexer, benchmark);
+criterion_main!(lexer);
--- src/code/mod.rs
+++
@@ -1,5 +0,0 @@
-//! The `code` module contains submodules handling linking and LLVM code generation
-pub mod linker;
-pub mod llvm;
-pub mod ns;
-pub use llvm::Compiler;
