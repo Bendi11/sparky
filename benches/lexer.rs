4d8509a6ec7492215a3f76a0a96586037aaf64a7
Bendi11 <bkliebmann@gmail.com>
2021-11-29 17:55:15 -0800

    re-organized files

--- benches/lexer.rs
+++ benches/lexer.rs
@@ -1,7 +1,22 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use spark::parse::lex::Lexer;
+
+const BENCH_SOURCE: [&str ; 17] = [r#"
+fun main i32 argc, []*char argv -> i32 {
+    let string := "testing\"\n\r "
+    mut num := 2412414141414 + 0xFFFABC13
+    num := num / 0b10101010 * 0o123
+    let result := string[31].field.memberfun 100, 31
+    (num)
+}
+"# ; 17];
 
 fn benchmark(c: &mut Criterion) {
-    c.bench_function("Lex 100 LOC", move |_| {});
+    let src = BENCH_SOURCE.join("");
+    c.bench_function("Lex 100 LOC", move |b| {
+        let mut lexer = Lexer::new(src.as_str());
+        b.iter(|| black_box(lexer.all(|_| true)))
+    });
 }
 
 criterion_group!(lexer, benchmark);
--- src/main.rs
+++
@@ -1,6 +0,0 @@
-pub mod util;
-pub mod parse;
-
-fn main() {
-
-}
