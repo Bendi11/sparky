5eff405f0b79cc7dd4ffbcef4356084d8714b44d
Bendi11 <bkliebmann@gmail.com>
2021-11-29 20:44:23 -0800

    Update grammar

---
+++ src/ast.rs
@@ -0,0 +1,8 @@
+//! Abstract syntax tree structures, the first representation of the program made by the compiler
+
+/// A node in an Abstract Syntax Tree
+#[derive(Clone, Debug)]
+pub enum AstNode {
+
+}
+
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,2 +1,24 @@
+use std::iter::Peekable;
+
+use self::lex::Lexer;
+
 pub mod lex;
 pub mod token;
+
+/// A structure consuming a token stream from a lexer and transforming it to an Abstract Syntax Tree
+#[derive(Clone, Debug)]
+pub struct Parser<'src> {
+    /// The token stream to consume tokens from 
+    toks: Peekable<Lexer<'src>>,
+}
+
+impl<'src> Parser<'src> {
+    /// Create a new `Parser` from the given source string
+    pub fn new(src: &'src str) -> Self {
+        Self {
+            toks: Lexer::new(src).peekable()
+        }
+    }
+
+    
+}
