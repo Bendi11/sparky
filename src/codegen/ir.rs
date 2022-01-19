514d80d01e6b33cab00f96cf57d81b25b46c6317
Ben <74798241+Bendi11@users.noreply.github.com>
2022-01-19 20:17:03 +0000

    updated IR types

--- src/codegen/ir.rs
+++ src/codegen/ir.rs
@@ -1,38 +1,42 @@
 use std::{rc::Rc, cell::RefCell};
 
 use hashbrown::HashMap;
+use quickscope::ScopeMap;
 
-use crate::{Symbol, arena::Index, ast::IntegerWidth};
+use crate::{Symbol, arena::{Index, Interner, Arena}, ast::IntegerWidth};
 
-pub type SparkModuleId = Index<SparkModule>;
 pub type TypeId = Index<Type>;
-pub type FunctionId = Index<Function>;
+pub type FunId = Index<Function>;
+pub type ModId = Index<SparkModule>;
+pub type DefId = Index<SparkDef>;
 
 /// Structure containing arenas holding all function definitions, 
 /// types, etc.
-#[derive(Clone, Debug)]
+#[derive(Clone,)]
 pub struct SparkCtx {
-    types: Rc<RefCell<Type>>,
-    modules: Rc<RefCell<SparkModule>>,
+    types: Interner<Type>,
+    modules: Arena<SparkModule>,
+    defs: Arena<SparkDef>,
+    root_module: SparkModule,
 }
 
 /// Structure containing type data plus a type ID that can be used to refer to the
 /// type
-#[derive(Clone, Debug)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub struct Type {
     data: TypeData,
     id: TypeId,
 }
 
 /// Function containing an entry basic block and argument data
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Function {
-    id: FunctionId,
+    id: FunId,
     name: Symbol,
 }
 
 /// A single type, either user-defined or predefined
-#[derive(Clone, Debug)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub enum TypeData {
     Integer {
         signed: bool,
@@ -52,9 +56,7 @@ pub enum TypeData {
     Struct {
         //Prevents interning from seeing two structure types as different
         name: Option<Symbol>,
-        fields: Vec<TypeId>,
-        //Map of field names to their indices in the `fields` vec
-        field_names: HashMap<Symbol, usize>,
+        fields: Vec<(TypeId, Option<Symbol>)>,
     },
     Enum {
         name: Option<Symbol>,
@@ -65,16 +67,31 @@ pub enum TypeData {
 }
 
 /// A function's type including argument types, return type, and flags
-#[derive(Clone, Debug)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub struct FunctionType {
     return_ty: TypeId,
     args: Vec<TypeId>,
 }
 
+
 /// Structure holding all definitions contained in a single module
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct SparkModule {
-    pub id: SparkModuleId,
+    id: ModId,
     pub name: Symbol,
-    pub functions: HashMap<Symbol, FunctionId>,
+    pub defs: ScopeMap<Symbol, DefId>,
+}
+
+/// Class containing a single definition in a module
+#[derive(Clone)]
+pub struct SparkDef {
+    id: DefId,
+}
+
+/// A single definition in the 
+#[derive(Clone, Copy)]
+pub enum DefData {
+    TypeDef(TypeId),
+    FunDef(FunId),
+    Import(DefId),
 }
