67f320ec7cb63de5430945f20c410da046fba800
Bendi11 <bkliebmann@gmail.com>
2022-01-19 10:20:28 -0800

    Begin work on IR code generation from AST

---
+++ src/codegen/ir.rs
@@ -0,0 +1,68 @@
+use std::{rc::Rc, cell::RefCell};
+
+use hashbrown::HashMap;
+
+use crate::{arena::Index, ast::IntegerWidth};
+
+pub type SparkModuleId = Index<SparkModule>;
+pub type TypeId = Index<Type>;
+pub type FunctionId = Index<Function>;
+
+/// Structure containing arenas holding all function definitions, 
+/// types, etc.
+pub struct SparkCtx {
+    types: Rc<RefCell<Type>>,
+    modules: Rc<RefCell<SparkModule>>,
+}
+
+/// Structure containing type data plus a type ID that can be used to refer to the
+/// type
+pub struct Type {
+    data: TypeData,
+    id: TypeId,
+}
+
+/// Function containing an entry basic block and argument data
+pub struct Function {
+    id: FunctionId,
+    name: Symbol,
+}
+
+/// A single type, either user-defined or predefined
+pub enum TypeData {
+    Integer {
+        signed: bool,
+        width: IntegerWidth,
+    },
+    Float {
+        doublewide: bool,
+    },
+    Bool,
+    Unit,
+    Pointer(TypeId),
+    Array {
+        element: TypeId,
+        len: u64,
+    },
+    Tuple(Vec<TypeId>),
+    Struct {
+        //Prevents interning from seeing two structure types as different
+        name: Option<Symbol>,
+        fields: Vec<TypeId>,
+        //Map of field names to their indices in the `fields` vec
+        field_names: HashMap<Symbol, usize>,
+    },
+    Enum {
+        name: Option<Symbol>,
+        parts: Vec<TypeId>,
+    },
+    Alias(TypeId),
+}
+
+
+/// Structure holding all definitions contained in a single module
+pub struct SparkModule {
+    pub id: SparkModuleId,
+    pub name: Symbol,
+
+}
